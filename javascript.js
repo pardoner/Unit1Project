dfe1dcd52223a5c4737d588247c4356800dec4a7
Dylan Wicker <dylanwicker@Dylans-MacBook-Air.local>
2023-11-10 20:13:14 -0500

    more changes

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-const string1 = "Hey! ";
-const string2 = "Get off ";
+const string1 = "Hey!";
+const string2 = "Get off";
 const string3 = "my page!";
 alert(string1 + " " + string2 + " " + string3);
